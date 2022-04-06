5d04bac1f8fcb54c20a2c7d2b9d1881c238a3f62
unknown <aisa.smajic27@gmail.com>
2022-04-06 15:09:11 +0200

    zadatak 1

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var myPet = {
+    name: "Riki",
+    species: "Buldog",
+    legs: 4,
+    friends: ["Miki", "Kiki"]
+    }
+    function myFunction(myObj){
+    return myObj;
+    }
+    // Only change code above this line
+    console.log(myFunction(myPet.name)); //Change this line
+    module.exports = { myPet, myFunction };
