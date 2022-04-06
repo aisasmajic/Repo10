11fa7225942810721561cc4cfcdc395435d04d3e
unknown <aisa.smajic27@gmail.com>
2022-04-06 15:53:52 +0200

    zadatak

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -5,9 +5,9 @@ var myClothes = {
     shoes: "Converse"
 };
 function myFunction(myObj){
-     hatValue = myObj.hat;
-     shirtValue = myObj.shirt;
-     shoesValue = myObj.shoes;
+    var hatValue = myObj.hat;
+    var shirtValue = myObj.shirt;
+     var shoesValue = myObj.shoes;
      return {hatValue, shirtValue, shoesValue};
 
 }
