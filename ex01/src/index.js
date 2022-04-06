57b99cb58a9da587d99bb10738fec8809ecae0ab
unknown <aisa.smajic27@gmail.com>
2022-04-06 15:28:05 +0200

    zadaci

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -4,10 +4,10 @@ var myPet = {
     species: "Buldog",
     legs: 4,
     friends: ["Miki", "Kiki"]
-    }
+    };
     function myFunction(myObj){
     return myObj;
     }
     // Only change code above this line
-    console.log(myFunction(myPet.name)); //Change this line
+    console.log(myFunction(myPet)); //Change this line
     module.exports = { myPet, myFunction };
